426f6174e1ac3e4e0ababb9518fbf52db2572e59
Joe Mogavero <jmogavero2@gmail.com>
2023-01-17 19:38:54 -0600

    basic working; figure out how to fix MD issue

--- dubstack-test/src/App.js
+++ dubstack-test/src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './template/Home'
+import Home from './template/Home';
+import SignUp from './template/SignUp';
+import SignIn from './template/SignIn';
+import Terms from './template/Terms';
+import Privacy from './template/Privacy';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/terms" element={<Terms />} />
+      <Route path="/privacy" element={<Privacy />} />
+
     </Routes>
   );
 }
